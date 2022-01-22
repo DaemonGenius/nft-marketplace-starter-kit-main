08275571e1191aaef7b8de97b6bb4586b407456c
Christian <christian.jhardy.steinmann@gmail.com>
2022-01-22 14:15:31 +0200

    configured truffle config started minting contract

---
+++ src/contracts/ERC721.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/*
+    Minting Function:
+    a. nft to point to an address
+    b. keep track of the token ids
+    c. keep track of token owners addresses to token ids
+    d. keep track of how many tokens an owner address has
+    e. create an event that emits a transfer log - contract address, where it is being minted to, the id
+
+    */
+contract ERC721 {
+    // Mapping from token id to the owner;
+    mapping(uint256 => address) private _tokenOwner;
+
+    // Mapping from owner to number of ownder tokens;
+    mapping(address => uint256) private _ownedTokensCount;
+
+    function _exists_check(uint tokenId) internal view returns(bool) {
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
+
+    // requires that the token does not already exist
+    modifier _exists(uint tokenId) {
+        require(!_exists_check(tokenId), 'ERC721: Token already minted');
+        _;
+    }
+
+    function _mint(address to, uint256 tokenId) internal _exists(tokenId) {
+        // requires address to be not zero
+        require(to != address(0), 'ERC721: mininting to the zero address');
+        // adding new address with token id for minting
+        _tokenOwner[tokenId] = to;
+        // keeping track of each address that is minting
+        _ownedTokensCount[to] += 1;
+    }
+}
